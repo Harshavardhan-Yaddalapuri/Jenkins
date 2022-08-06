f6f9c6f8be63a6e02414414decfbd5a6fde6f4d9
Harshavardhan <52969375+Harshavardhan-Yaddalapuri@users.noreply.github.com>
2022-08-07 00:20:23 +0530

    updated the println Statement

--- Jenkins.java
+++ Jenkins.java
@@ -1,5 +1,5 @@
 public class Jenkins {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!\n This is to test Jenkins Job using Git SCM!");
     }
 }
